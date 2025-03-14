3bc7e6df1bc89b8d23e518aad59eb2513ce804cf
Rico <ekirschke@web.de>
2025-03-14 16:39:15 +0100

    add hamster-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HamsterCardComponent } from "./hamster-card/hamster-card.component";
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, HamsterCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/hamster-card/hamster-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hamster-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './hamster-card.component.html',
+  styleUrl: './hamster-card.component.scss'
+})
+export class HamsterCardComponent implements OnInit {
+
+  constructor() { }
+  
+  ngOnInit(): void {  
+  }
+
+}
